6d2d6aa433d8fb6bce5d1bdc3a4c36835a5f3f24
Bidhu1024 <bidhu1024av@gmail.com>
2023-11-20 20:43:57 +0530

    dom manipulation

---
+++ script.js
@@ -0,0 +1,22 @@
+const body = document.body;
+//const div = document.createElement('div')
+//div.innerText="Hello"
+//div.textContent="world"
+//div.innerHTML="<strong>Hello</strong>"
+
+// const strong = document.createElement("strong")
+// strong.textContent = "Hello";
+// div.append(strong)
+
+
+
+//body.append(div)
+
+//removing elements from screen
+const div = document.querySelector("div")
+const hello = document.querySelector("#name")
+const bye = document.querySelector("#name2")
+
+bye.remove()
+
+//
