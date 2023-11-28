8146222af8a36d2912c4f7a729a5ae9bdff2a175
Bidhu1024 <bidhu1024av@gmail.com>
2023-11-28 09:15:00 +0530

    fixes

--- Callback.js
+++ Callback.js
@@ -1,9 +1,19 @@
-const num=(num1,num2)=>{
-    console.log(num1+num2);
+// const num=(num1,num2)=>{
+//     console.log(num1+num2);
+// }
+
+// setInterval(()=>{
+//     console.log("timeout");
+// },1000)
+
+// num(2,6)
+ 
+
+function greet(name,callback){
+    console.log("Hi " + name)
+    callback()
 }
-
-setInterval(()=>{
-    console.log("timeout");
-},1000)
-
-num(2,6)
+function callme(){
+    console.log('I am called')
+}
+greet('John',callme)
